4c54fb06e53869e46e2183addf7dd8c1a7dd91f1
Redchin Daniil <redchindaniil@gmail.com>
2025-02-08 23:03:40 +0700

    Client + server bugfix

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::ToSocketAddrs;
+
+use tokio::net::TcpStream;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = match std::env::args().skip(1).next() {
+        Some(x) => x,
+        None => {
+            eprintln!("Usage: ./client <address>:<port>");
+            return Ok(());
+        }
+    };
+    let addr = addr.to_socket_addrs()?
+        .next()
+        .ok_or("Provided address is invalid")?;
+
+    println!("Connecting to server {addr}");
+
+    let stream = TcpStream::connect(addr).await?;
+    let _ = stream.set_nodelay(true);
+
+    
+
+    Ok(())
 }
